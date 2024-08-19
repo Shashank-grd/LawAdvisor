a041368e824814c2ec9f2c89952ee303a21fd89d
Shashank-grd <shashank.2grd@gmail.com>
2024-08-19 18:47:31 +0530

    Navigator

--- lib/utils/variable.dart
+++ lib/utils/variable.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:lawadvisor/screens/home.dart';
+import 'package:lawadvisor/screens/profilescreen.dart';
+import 'package:lawadvisor/screens/search-screen.dart';
 
 
 List<Widget> homeScreenItems = [
   HomeScreen(),
-  Text("search"),
-  Text("noti"),
+  SearchScreen(),
   Text("chat"),
+  ProfileScreen()
 ];
