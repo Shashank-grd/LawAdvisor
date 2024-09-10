09bc0867838237e878753e7207c02fc389bc8949
Shashank-grd <shashank.2grd@gmail.com>
2024-09-10 17:52:56 +0530

    lawyer

--- lib/utils/variable.dart
+++ lib/utils/variable.dart
@@ -7,6 +7,12 @@ import 'package:lawadvisor/screens/search-screen.dart';
 List<Widget> homeScreenItems = [
   HomeScreen(),
   SearchScreen(),
-  Text("chat"),
+  Center(child: Text("Chatbot has also been created, all that remains is to connect the app with Chatbot, and the Jupyter file has also been attached.",
+  style: TextStyle(
+    wordSpacing: 5,
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+  ),)),
   ProfileScreen()
 ];
+
