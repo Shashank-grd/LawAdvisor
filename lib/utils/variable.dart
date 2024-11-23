021bf30d80350abdfada095e0f4b12b87bd00605
Shashank-grd <shashank.2grd@gmail.com>
2024-11-23 20:31:54 +0530

    chatscreen added

--- lib/utils/variable.dart
+++ lib/utils/variable.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lawadvisor/screens/chatai.dart';
 import 'package:lawadvisor/screens/home.dart';
 import 'package:lawadvisor/screens/profilescreen.dart';
 import 'package:lawadvisor/screens/search-screen.dart';
@@ -7,12 +8,7 @@ import 'package:lawadvisor/screens/search-screen.dart';
 List<Widget> homeScreenItems = [
   HomeScreen(),
   SearchScreen(),
-  Center(child: Text("Chatbot has also been created, all that remains is to connect the app with Chatbot, and the Jupyter file has also been attached.",
-  style: TextStyle(
-    wordSpacing: 5,
-    fontSize: 18,
-    fontWeight: FontWeight.bold,
-  ),)),
+  ChatPage(),
   ProfileScreen()
 ];
 
